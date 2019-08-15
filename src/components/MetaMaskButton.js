a57aae08ed723e9e6c213efc4958f9b7582a4700
geleeroyale <krassvs@gmail.com>
2019-08-15 23:23:54 +0200

    initial styling

--- src/components/MetaMaskButton.js
+++ src/components/MetaMaskButton.js
@@ -3,76 +3,100 @@ import React, { useContext } from "react";
 import { connect } from "react-redux";
 import MetaMaskContext from "./MetaMask";
 
-const MetaMaskButton = ({onWeb3Ready}) => {
-    const { web3, accounts, error, awaiting, openMetaMask } = useContext(
-        MetaMaskContext,
-    );
+const MetaMaskButton = ({ onWeb3Ready }) => {
+  const { web3, accounts, error, awaiting, openMetaMask } = useContext(
+    MetaMaskContext
+  );
 
-    function handleButtonClick() {
-        alert(`Web3 (${web3.version}) is enabled`);
-    }
+  function handleButtonClick() {
+    alert(`Web3 (${web3.version}) is enabled`);
+  }
 
-    if (error && error.message === "MetaMask not installed") {
-        return (
-            <a href="https://metamask.io/" target="_blank" rel="noopener noreferrer">
-                Install MetaMask
+  if (error && error.message === "MetaMask not installed") {
+    return (
+      <a href="https://metamask.io/" target="_blank" rel="noopener noreferrer">
+        Install MetaMask
       </a>
-        );
-    } else if (error && error.message === "User denied account authorization") {
-        return (
-            <button type="button" className="button is-success" onClick={openMetaMask}>
-                Please allow MetaMask to connect.
+    );
+  } else if (error && error.message === "User denied account authorization") {
+    return (
+      <button
+        type="button"
+        className="button is-success"
+        onClick={openMetaMask}
+      >
+        Please allow MetaMask to connect.
       </button>
-        );
-    } else if (error && error.message === "MetaMask is locked") {
-        return (
-            <button type="button" className="button is-success" onClick={openMetaMask}>
-                Please allow MetaMask to connect.
+    );
+  } else if (error && error.message === "MetaMask is locked") {
+    return (
+      <button
+        type="button"
+        className="button is-success"
+        onClick={openMetaMask}
+      >
+        Please allow MetaMask to connect.
       </button>
-        );
-    } else if (error) {
-        return (
-            <button type="button" className="button is-success" onClick={openMetaMask}>
-                UNHANDLED ERROR: {error.message}
-            </button>
-        );
-    } else if (!web3 && awaiting) {
-        return (
-            <button type="button" className="button is-success" onClick={openMetaMask}>
-                MetaMask is loading...
+    );
+  } else if (error) {
+    return (
+      <button
+        type="button"
+        className="button is-success"
+        onClick={openMetaMask}
+      >
+        UNHANDLED ERROR: {error.message}
       </button>
-        );
-    } else if (!web3) {
-        return (
-            <button type="button" className="button is-success" onClick={openMetaMask}>
-                Please open and allow MetaMask
+    );
+  } else if (!web3 && awaiting) {
+    return (
+      <button
+        type="button"
+        className="button is-success"
+        onClick={openMetaMask}
+      >
+        MetaMask is loading...
       </button>
-        );
-    } else if (accounts.length === 0) {
-        return <button type="button">className="button is-success" No Wallet 🦊</button>;
-    } else {
-        // `web3` and `account` loaded 🎉
-        onWeb3Ready();
-        return (
-            <button type="button" className="button is-success" onClick={handleButtonClick}>
-                <code>{accounts[0]}</code> 🦊 (v: {web3.version.api})
+    );
+  } else if (!web3) {
+    return (
+      <button
+        type="button"
+        className="button is-success"
+        onClick={openMetaMask}
+      >
+        Please open and allow MetaMask
       </button>
-        );
-    }
-}
-
+    );
+  } else if (accounts.length === 0) {
+    return (
+      <button type="button">className="button is-success" No Wallet 🦊</button>
+    );
+  } else {
+    // `web3` and `account` loaded 🎉
+    onWeb3Ready();
+    return (
+      <button
+        type="button"
+        className="button is-warning"
+        onClick={handleButtonClick}
+      >
+        <code>{accounts[0]}</code> 🦊 (v: {web3.version.api})
+      </button>
+    );
+  }
+};
 
 const mapStateToProps = state => {
-    return {       
-    };
+  return {};
 };
 const mapDispachToProps = dispatch => {
-    return {
-        onWeb3Ready: () => dispatch({ type: "WEB3_AVAILABLE" }),
-    };
+  return {
+    onWeb3Ready: () => dispatch({ type: "WEB3_AVAILABLE" })
+  };
 };
 
 export default connect(
-    mapStateToProps,
-    mapDispachToProps
+  mapStateToProps,
+  mapDispachToProps
 )(MetaMaskButton);
