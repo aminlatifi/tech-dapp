f5f7277a116f8fe65802aa8364a4e0a8d67ad41b
theDoubleBubble <bulepaja@gmail.com>
2020-06-04 16:25:18 +0200

    Styled Progress bar for both scenarios

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -5,6 +5,25 @@ import BN from "bn.js";
 import "./Slider.sass";
 
 const Slider = ({ softCap, hardCap, totalReceived }) => {
+  const getProgressBarStyles = ({ _softCap, _hardCap, _totalReceived }) => {
+    let fill, shadow, full;
+
+    if (_totalReceived < _softCap) {
+      fill = (_totalReceived / _hardCap) * 100;
+      shadow = ((_softCap - _totalReceived) / _hardCap) * 100;
+      full = ((_hardCap - _softCap) / _hardCap) * 100;
+    } else {
+      fill = (_softCap / _hardCap) * 100;
+      shadow = ((_totalReceived - _softCap) / _hardCap) * 100;
+      full = ((_hardCap - _totalReceived) / _hardCap) * 100;
+    }
+
+    return {
+      fill,
+      shadow,
+      full,
+    };
+  };
   const _softCap =
     softCap && softCap.div(new BN("1000000000000000000")).toString(10);
   const _hardCap =
@@ -12,6 +31,19 @@ const Slider = ({ softCap, hardCap, totalReceived }) => {
   const _totalReceived =
     totalReceived &&
     totalReceived.div(new BN("1000000000000000000")).toString(10);
+  // Just for testing purposes
+  // const _softCap = 10;
+  // const _hardCap = 15;
+  // const _totalReceived = 12;
+  // const _softCap = 12;
+  // const _hardCap = 15;
+  // const _totalReceived = 10;
+
+  const progressBarStyles = getProgressBarStyles({
+    _softCap,
+    _hardCap,
+    _totalReceived,
+  });
 
   return (
     <div className="slider">
@@ -31,17 +63,37 @@ const Slider = ({ softCap, hardCap, totalReceived }) => {
 
         <div className="level-right">
           <div className="level-item">
-            <p className="subtitle is-6">{_softCap} DAI</p>
+            <p className="subtitle is-6 right">{_softCap} DAI</p>
           </div>
           <div className="level-item">
-            <p className="subtitle is-6">{_hardCap} DAI</p>
+            <p className="subtitle is-6 right">{_hardCap} DAI</p>
           </div>
         </div>
       </div>
       <div className="progress">
-        <div className="progress-bar fill"></div>
-        <div className="progress-bar shadow"></div>
-        <div className="progress-bar full"></div>
+        <div
+          className={"progress-bar fill"}
+          style={{ width: `${progressBarStyles.fill}%` }}
+        ></div>
+        <div
+          className="progress-bar shadow"
+          style={
+            _softCap - _totalReceived > 0
+              ? {
+                  width: `${progressBarStyles.shadow}%`,
+                  backgroundColor: "#4A5D6F",
+                }
+              : {
+                  width: `${progressBarStyles.shadow}%`,
+                  background:
+                    "linear-gradient(270deg, #8E2DE2 0%, #4A00E0 100%)",
+                }
+          }
+        ></div>
+        <div
+          className="progress-bar full"
+          style={{ width: `${progressBarStyles.full}%` }}
+        ></div>
       </div>
 
       <div className="level">
@@ -52,10 +104,10 @@ const Slider = ({ softCap, hardCap, totalReceived }) => {
         </div>
         <div className="level-right">
           <div className="level-item">
-            <p className="subtitle is-7">Soft Cap</p>
+            <p className="subtitle is-7 right">Soft Cap</p>
           </div>
           <div className="level-item">
-            <p className="subtitle is-7">Hard Cap</p>
+            <p className="subtitle is-7 right">Hard Cap</p>
           </div>
         </div>
       </div>
